ae2e14568f650252575bf2eaaeacf04840e4a438
Diogo <93263797+ogoiddev@users.noreply.github.com>
2022-09-21 17:55:43 -0300

    importacao md5 antes do ../erro

--- back-end/src/services/registerServices.js
+++ back-end/src/services/registerServices.js
@@ -1,6 +1,7 @@
+const md5 = require('md5');
 const ErrorProvider = require('../error');
 const { users } = require('../database/models/index');
-const md5 = require('md5');
+
 
 const registerService = async ({ name, email, password }) => {
   const passConverted = md5(password).toString();
